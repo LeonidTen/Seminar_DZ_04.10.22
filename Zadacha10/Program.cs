9f2afb7558380df6410518093988280a8e407db0
leonid <tenleonid@gmail.com>
2022-10-06 12:28:52 +0600

    DZ10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 999 && number > 99)
+{
+    number = number / 10;
+    Console.WriteLine($"Вторая цифра числа {number = number % 10}");
+}
+else
+Console.WriteLine("Введенно не трехзначное число");
