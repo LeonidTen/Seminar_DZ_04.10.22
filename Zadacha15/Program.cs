19981c286e0fb47f6c4c9b46c32270c81773c0c6
leonid <tenleonid@gmail.com>
2022-10-07 10:33:49 +0600

    DZ15

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите число от 1 до 7:");
+string DayOfWeek = Console.ReadLine();
+
+if (DayOfWeek == "1")
+Console.WriteLine("Понедельник");
+
+else if (DayOfWeek == "2")
+Console.WriteLine("Вториник");
+
+else if (DayOfWeek == "3")
+Console.WriteLine("Среда");
+
+else if (DayOfWeek == "4")
+Console.WriteLine("Четверг");
+
+else if (DayOfWeek == "5")
+Console.WriteLine("Пятница");
+
+else if (DayOfWeek == "6")
+Console.WriteLine("Суббота, является выходным днем");
+
+else if (DayOfWeek == "7")
+Console.WriteLine("Воскресенье, является выходным днем");
